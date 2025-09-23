5e72a30675f7667603d359d63939ba8dc6624833
LuHuyen <152854551+ArthasTheShibe@users.noreply.github.com>
2025-09-23 21:40:55 +0200

    Vector

--- vector.cpp
+++ vector.cpp
@@ -17,14 +17,46 @@ private:
     T m_x{};
     T m_y{};
     T m_z{};
-    std::array<T, 3> m_vector{m_x, m_y, m_z};
+    std::vector<T, 3> m_vector{m_x, m_y, m_z};
 
 public:
-} Vector operator+(const Vector &v1, const Vector &v2)
-{
-    Vector v{};
-    for (T element : m_vector)
-    {
-        element =
-    }
+} ;
+template <typename T>
+Vector operator+(const Vector &v1, const Vector &v2)
+{   
+    static_assert(v1.size() == v2.size(), "Vectors don't have the same dimension.");
+    Vector result{};
+    for (std::size_t i{0}; i < v1.size(); ++v)
+        result[i] = v1[i]+v2[i];  
+    return result;
 }
+
+template <typename T>
+Vector operator*( T k, const Vector &v)
+{   
+     Vector result{};
+    for (std::size_t i{0}; i < v.size(); ++v)
+        result[i] = k * v[i];  
+    return result;
+}
+
+template <typename T>
+Vector operator*(const Vector &v, T k)
+{    
+    return k * v;
+}
+
+template <typename T>
+Vector operator-(const Vector &v)
+{    
+    return -1 * v;
+}
+
+template <typename T>
+Vector operator-(const Vector &v1, const Vector& v2)
+{    
+    return v1 + (-v2);
+}
+
+Vector dotProduct(){}
+Vector crossProduct(){}
